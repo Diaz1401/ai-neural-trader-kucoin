2980200467eb907d1d90a0b2638d0ed6002214c8
Diaz1401 <diaznura@outlook.com>
2023-12-27 19:31:14 +0700

    use numpy

--- random_forest.py
+++ random_forest.py
@@ -14,13 +14,14 @@ import telebot
 import threading
 
 BASE = 'USDT'
-CRYPTO = 'ETH'
+CRYPTO = 'BTC3L'
 # BASE = 'USDC'
 # CRYPTO = 'USDT'
 SYMBOL = CRYPTO + '/' + BASE
 TIMEFRAME = '15m'
 SLEEP = 1
-TEST  = False
+TEST  = False # test accuracy
+LOOP = False # test with loop
 
 app = Flask(__name__)
 
@@ -34,7 +35,6 @@ PASSWORD = os.getenv('PASSWORD')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
-
 def initialize_exchange():
     exchange_name = 'kucoin'
     exchange = getattr(ccxt, exchange_name)()
@@ -45,88 +45,93 @@ def initialize_exchange():
     return exchange
 
 def train_model(data):
-    features = []
-    target = []
-    print(len(data))
-    print(data[-1])
-    for i in range(0, len(data)-1):
-        features.append(data[i][1:])
-        target.append(1 if data[i+1][-1] > data[i][-1] else 0)
-    features = np.array(features)
-    target = np.array(target)
-    model = RandomForestClassifier(criterion='entropy', n_estimators=1000, max_depth=1000, max_features=150, n_jobs=-1)
-
+    data = np.array(data)
+    features = data[:-1, 1:]  # Extracting features
+    target = np.where(data[1:, -1] > data[:-1, -1], 1, 0)  # Generating target values
+    model = RandomForestClassifier(criterion='entropy', n_estimators=5000, max_depth=1000, max_features=None, n_jobs=-1)
     model = model.fit(features, target)
     return model
 
-def calculate_sma_ema(prices):
-    window_sizes = [i for i in range(2, 51)]
-    sma_ema = []
+def calculate_sma(prices):
+    window_sizes = range(50, 4, -5)
+    sma_values = [round(np.mean(prices[-window:]), 10) for window in window_sizes]
+    return sma_values
+
+def calculate_ema(prices):
+    window_sizes = range(50, 4, -5)
+    ema_values = []
     for window in window_sizes:
         alpha = 2 / (window + 1)
         initial_sma = np.mean(prices[-window:])
-        ema_values = [initial_sma]
+        ema_value = initial_sma
         for price in prices[-window + 1:]:
-            ema_value = alpha * price + (1 - alpha) * ema_values[-1]
-            ema_values.append(ema_value)
-        sma_ema.append(round(ema_values[-1], 10))
-        sma_ema.append(round(np.sum(prices[-window:]) / window, 10))
-    return sma_ema
+            ema_value = alpha * price + (1 - alpha) * ema_value
+        ema_values.append(round(ema_value, 10))
+    return ema_values
+
+def calculate_bollinger_bands(prices):
+    upper_band = []
+    lower_band = []
+    for i in range(50, 4, -5):
+        # Calculate rolling mean using numpy
+        rolling_mean = np.mean(prices[:i])
+        # Calculate rolling standard deviation using numpy
+        rolling_std = np.std(prices[:i], ddof=1)  # ddof=1 for Bessel's correction
+        # Calculate upper and lower Bollinger Bands
+        upper_band.append(rolling_mean + 2 * rolling_std)
+        lower_band.append(rolling_mean - 2 * rolling_std)
+    return upper_band, lower_band
 
 def calculate_rsi(prices):
     def calculate_single_rsi(prices, window_size):
-        gains = []
-        losses = []
-
-        for i in range(1, len(prices)):
-            diff = prices[i] - prices[i - 1]
-            if diff >= 0:
-                gains.append(diff)
-                losses.append(0)
-            else:
-                gains.append(0)
-                losses.append(abs(diff))
-
-        avg_gain = sum(gains[:window_size]) / window_size
-        avg_loss = sum(losses[:window_size]) / window_size
+        gains = np.where(np.diff(prices) >= 0, np.diff(prices), 0)
+        losses = np.where(np.diff(prices) < 0, -np.diff(prices), 0)
+        avg_gain = np.mean(gains[:window_size])
+        avg_loss = np.mean(losses[:window_size])
         rs = avg_gain / avg_loss if avg_loss != 0 else 0
         rsi = 100 - (100 / (1 + rs))
         rsi = (rsi - 50) * 2
         return rsi
-
-    window_sizes = [i for i in range(2, 51)]
+    window_sizes = range(50, 4, -5)
     rsi_values = [calculate_single_rsi(prices, frame_size) for frame_size in window_sizes]
     return rsi_values
 
-def split_data(data, ratio=0.95):
-    # data = data[:-150]
-    # data = data[-1000:]
+def split_data(data, ratio=0.8):
+    cut = 100
+    take = 1000
+    # data = data[:-cut] # Delete last 'cut' data
+    data = data[-take:] # Only take last 'take' data (max 1500)
     format = []
     for i in range(49, len(data)):
         temp = [row[4] for row in data[i-49:i+1]]
-        sma_ema = calculate_sma_ema(temp)
+        sma = calculate_sma(temp)
+        ema = calculate_sma(temp)
         rsi = calculate_rsi(temp)
-        format.append([data[i][0], *sma_ema[:], *rsi[:], data[i][4]])
+        upper_bollinger, lower_bollinger = calculate_bollinger_bands(temp)
+        format.append([data[i][0], *sma[:], *ema[:], *rsi[:], *upper_bollinger[:], *lower_bollinger[:], data[i][4]])
     split_index = int(len(format) * ratio)
     train_data = format[:split_index]
     test_data = format[split_index:]
     # train_data = format
     # test_data = format
-    print(f'Train: {len(train_data)}\nTest: {len(test_data)}')
+    if TEST:
+        print(f'Train: {len(train_data)}\nTest: {len(test_data)}')
+    else:
+        print(f'Full: {len(format)}')
     return train_data, test_data, format
 
 def predict_market_direction(model, data):
-    print(len(data[-1]))
-    features = np.array(data[-1][1:]).reshape(-1, 148)
+    length = len(data[-1]) - 1
+    features = np.array(data[-1][1:]).reshape(-1, length)
     prediction = model.predict_proba(features)[0][1]
     return prediction
 
 def plot_predictions(timestamps, predictions, prices):
     # Convert timestamps to dates
     if TEST:
-        dates = [datetime.fromtimestamp(ts / 1000).strftime('%H\n%M\n%S') for ts in timestamps]
+        dates = [datetime.fromtimestamp(ts / 1000).strftime('%H:%M:%S') for ts in timestamps]
     else:
-        dates = [ts.strftime('%H\n%M\n%S') for ts in timestamps]
+        dates = [ts.strftime('%H:%M:%S') for ts in timestamps]
     plt.figure(figsize=(10, 6))
 
     plt.subplot(2, 1, 1)  # Creating the first subplot for predictions
@@ -135,7 +140,7 @@ def plot_predictions(timestamps, predictions, prices):
     plt.xlabel('Time')
     plt.ylabel('Predictions')
     plt.grid(True)
-    #plt.xticks(rotation='vertical')
+    plt.xticks(rotation='vertical')
     
     plt.subplot(2, 1, 2)  # Creating the second subplot for prices
     plt.plot(dates, prices, linestyle='-', color='r')
@@ -143,7 +148,7 @@ def plot_predictions(timestamps, predictions, prices):
     plt.xlabel('Time')
     plt.ylabel('Prices')
     plt.grid(True)
-    #plt.xticks(rotation='vertical')
+    plt.xticks(rotation='vertical')
     
     plt.tight_layout()
     plt.savefig('static/output.png')  # Save the plot image
@@ -182,14 +187,9 @@ def main():
 
             if TEST:
                 model = train_model(train_data)
-                test_features = []
-                test_target = []
-                for i in range(0, len(test_data)-1):
-                    test_features.append(test_data[i][1:])
-                    test_target.append(1 if test_data[i+1][-1] > test_data[i][-1] else 0)
-                test_features = np.array(test_features)
-                test_target = np.array(test_target)
-
+                test_data = np.array(test_data)
+                test_features = test_data[:-1, 1:]  # Extracting features
+                test_target = np.where(test_data[1:, -1] > test_data[:-1, -1], 1, 0)  # Generating target values
                 # Checking test accuracy
                 test_predictions = model.predict(test_features)
                 test_accuracy = model.score(test_features, test_target)
@@ -204,33 +204,34 @@ def main():
                 max_up = 0
                 max_down = 0
                 total_percent = 0
-                for row in test_data:
-                    if loop < len(test_data)-5:
-                        future_prices.append([test_data[loop+1][-1], test_data[loop+2][-1], test_data[loop+3][-1], test_data[loop+4][-1], test_data[loop+5][-1]])
-                        max_up = ((max(future_prices[-1]) - row[-1]) / row[-1])
-                        max_down = ((min(future_prices[-1]) - row[-1]) / row[-1])
+                if LOOP:
+                    for row in test_data:
+                        if loop < len(test_data)-5:
+                            future_prices.append([test_data[loop+1][-1], test_data[loop+2][-1], test_data[loop+3][-1], test_data[loop+4][-1], test_data[loop+5][-1]])
+                            max_up = ((max(future_prices[-1]) - row[-1]) / row[-1])
+                            max_down = ((min(future_prices[-1]) - row[-1]) / row[-1])
 
-                        # Check if the price increased or decreased
-                        print('# Show increase/decrease potential in next 5 move')
-                        print(f"==> Max increase by {round(max_up * 100, 3)}%")
-                        print(f"==> Max decrease by {round(max_down * 100, 3)}%")
-                    # Predict market direction for each row in test data
-                    prediction = predict_market_direction(model, [row])
+                            # Check if the price increased or decreased
+                            print('# Show increase/decrease potential in next 5 move')
+                            print(f"==> Max increase by {round(max_up * 100, 3)}%")
+                            print(f"==> Max decrease by {round(max_down * 100, 3)}%")
+                        # Predict market direction for each row in test data
+                        prediction = predict_market_direction(model, [row])
 
-                    timestamps.append(row[0])  # Assuming timestamp is the first element
-                    predictions.append(prediction)  # Append prediction
-                    prices.append(row[-1])  # Assuming price data is in the fifth column (adjust if necessary)
-                    if prediction > 0.5 and loop < len(test_data)-5:
-                        if any(x > row[-1] for x in future_prices[-1]):
-                            win += 1
-                        else:
-                            loss += 1
-                        total_percent += round(max_up*100, 2)
-                    print(f'BALANCE ${balance}')
-                    print(f'Loop    : {loop}\nGuess   : {win + loss}\nWin     : {win}\nLoss    : {loss}\nMax     : {total_percent}\n')
-                    loop += 1
+                        timestamps.append(row[0])  # Assuming timestamp is the first element
+                        predictions.append(prediction)  # Append prediction
+                        prices.append(row[-1])  # Assuming price data is in the fifth column (adjust if necessary)
+                        if prediction > 0.5 and loop < len(test_data)-5:
+                            if any(x > row[-1] for x in future_prices[-1]):
+                                win += 1
+                            else:
+                                loss += 1
+                            total_percent += round(max_up*100, 2)
+                        print(f'BALANCE ${balance}')
+                        print(f'Loop    : {loop}\nGuess   : {win + loss}\nWin     : {win}\nLoss    : {loss}\nMax     : {total_percent}\n')
+                        loop += 1
+                    print(f'WIN {(win / (win + loss)) * 100}%')
 
-                print(f'WIN {(win / (win + loss)) * 100}%')
                 print(f"Mean Squared Error: {mse}")
                 print(f"Mean Absolute Error: {mae}")
                 print(f"Accuracy: {round(test_accuracy * 100, 1)}%")
@@ -260,16 +261,16 @@ def main():
                 prices.append(full_data[-1][-1])
                 if prediction > 0.6: # buy
                     #exchange.create_order(SYMBOL, 'limit', 'buy', crypto_amount, midpoint)
-                    print(f'BUY ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
+                    print(f'BUY ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
                     #print(f'Bought {crypto_amount} {CRYPTO} at {midpoint} {BASE}\nScore: {round(prediction, 3)}')
                     #send_telegram_message(f'Bought {crypto_amount} {CRYPTO} at {midpoint} {BASE}\nScore: {round(prediction, 3)}')
-                    send_telegram_message(f'BUY ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
+                    send_telegram_message(f'BUY ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
                 elif prediction < 0.4: # sell
-                    print(f'SELL ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
-                    send_telegram_message(f'SELL ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
+                    print(f'SELL ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
+                    send_telegram_message(f'SELL ZONE !!!\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
                 else:
-                    print(f'Neutral\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
-                    send_telegram_message(f'Neutral\nCoin: {CRYPTO}\nPrice: {round(midpoint, 2)}\nScore: {round(prediction, 3)}')
+                    print(f'Neutral\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
+                    send_telegram_message(f'Neutral\nCoin: {CRYPTO}\nPrice: {round(midpoint, 10)}\nScore: {round(prediction, 3)}')
 
             plot_thread = threading.Thread(target=plot_predictions, args=(timestamps[-50:], predictions[-50:], prices[-50:]))
             plot_thread.start()
